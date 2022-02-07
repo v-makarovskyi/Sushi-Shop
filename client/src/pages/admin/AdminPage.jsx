9b70cf881db7741974947f07086c02b94c473fbd
Vladimir <bigcorvet@gmail.com>
2022-02-07 09:14:15 +0200

    Подготовлено к верстке

--- client/src/pages/admin/AdminPage.jsx
+++ client/src/pages/admin/AdminPage.jsx
@@ -1,17 +1,19 @@
-import './adminpage.scss'
+import "./adminpage.scss";
 import { Categories } from "../../conponents/categories/Categories";
 import { Footer } from "../../conponents/footer/Footer";
 import { Topbar } from "../../conponents/topbar/Topbar";
 
-import React from 'react'
+import React from "react";
 
 export const AdminPage = () => {
-    return (
-        <div className='admin-container'>
-            <Topbar />
-            <Categories />
-            <h2>ADMIN</h2>
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div className="admin-container">
+      <Topbar />
+      <Categories />
+      <div className="content">
+        <h2>ADMINPAGE</h2>
+      </div>
+      <Footer />
+    </div>
+  );
+};
